bbab54e3ac0473822366800e54e37cc3365cab2b
Mauricio Hidalgo <mhidalgodg@gmail.com>
2022-07-02 22:34:29 -0600

    p carrito compras 1

---
+++ 15-PROYECTO-Carrito/js/app.js
@@ -0,0 +1,34 @@
+//Variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners(){
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+//Funciones
+function agregarCurso(e){
+    e.preventDefault();
+    if (e.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+
+function leerDatosCurso(curso){
+    //console.log(curso);
+
+    //Crear objeto con contenido de curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        nombre: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'), 
+        cantidad: 1
+    }
+
+    console.log(infoCurso);
+}
